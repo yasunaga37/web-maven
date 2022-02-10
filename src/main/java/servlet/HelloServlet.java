65d12536810fc9342d301a389db1e869fd9aac66
yasunaga37 <yasunaga37@gmail.com>
2022-02-10 09:57:38 +0900

    first-commit

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// レスポンスのコンテンツタイプおよびエンコーディング方式を指定
+		response.setContentType("text/html; charset=UTF-8");
+		// レスポンス書き出し用オブジェクトの取得
+		PrintWriter pw = response.getWriter();
+		// レスポンスの書き出し
+		pw.println("<!DOCTYPE html><html>");
+		pw.println("<head><title>Hello タコピ！</title></head>");
+		pw.println("<body>Hello タコピ！</body>");
+		pw.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
